102985550c094fdecd4c0f88ebbbbe8c82cf6a99
Jaehyuk Lee <jhlumd@gmail.com>
2020-05-06 04:23:59 -0700

    Create simple axes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// const dataset = [1, 2, 3, 4, 5];
+/*
+const dataset = [1, 2, 3, 4, 5];
 
-// d3.select("body")
-//   .selectAll("p")
-//   .data(dataset)
-//   .enter()
-//   .append("p")
-//   .text((d) => d);
+d3.select("body")
+  .selectAll("p")
+  .data(dataset)
+  .enter()
+  .append("p")
+  .text((d) => d);
+*/
 
+/*
 const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 const dataset2 = dataset.map((el) => el / 10);
 
@@ -45,3 +48,35 @@ const text = svg
   .attr("y", (d, i) => svgHeight - d - 2)
   .attr("x", (d, i) => barWidth * i)
   .attr("fill", "#A64C38");
+*/
+
+const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+const svgWidth = 500;
+const svgHeight = 300;
+
+const svg = d3.select("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+const xScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([0, svgWidth]);
+
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([svgHeight, 0]);
+
+const xAxis = d3.axisBottom()
+  .scale(xScale);
+
+const yAxis = d3.axisLeft()
+  .scale(yScale);
+
+svg.append("g")
+  .attr("transform", `translate(50, ${svgHeight - 20})`)
+  .call(xAxis);
+
+svg.append("g")
+  .attr("transform", "translate(50, 10)")
+  .call(yAxis);
