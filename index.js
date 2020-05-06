3a5df9f9f5994678161b20a9b458f66982dad88e
Jaehyuk Lee <jhlumd@gmail.com>
2020-05-06 03:40:36 -0700

    Create simple bar chart

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const dataset = [1, 2, 3, 4, 5];
+// const dataset = [1, 2, 3, 4, 5];
 
-d3.select("body")
-  .selectAll("p")
+// d3.select("body")
+//   .selectAll("p")
+//   .data(dataset)
+//   .enter()
+//   .append("p")
+//   .text((d) => d);
+
+const dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+const svgWidth = 500;
+const svgHeight = 300;
+const barPadding = 5;
+const barWidth = svgWidth / dataset.length;
+
+const svg = d3.select("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+const barChart = svg.selectAll("rect")
   .data(dataset)
   .enter()
-  .append("p")
-  .text("D3 is working..");
+  .append("rect")
+  .attr("y", (d) => svgHeight - d)
+  .attr("height", (d) => d)
+  .attr("width", barWidth - barPadding)
+  .attr("transform", (d, i) => `translate(${barWidth * i}, 0 )`);
